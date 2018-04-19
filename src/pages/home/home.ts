d02b50b13a445045889af5f8246b8f5829b2b58f
frédéric <fdamour121@gmail.com>
2018-04-19 10:20:16 +0200

    création d'un objet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,29 +2,56 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Observable } from 'rxjs';
 
-
-
-
-
-
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
-
-  titre ='app works';
-  firsName ="robert";
-  name = "dupont";
-
+//passage d'information du composant vers template
+//interpolation par {{}} ou []=
+  private titre: string;
+  private firsName: string;
+  private name: string;
+  private collection: [any];
+  public newItem:  {reference:string, name:string, state:number};
 
 
   constructor(public navCtrl: NavController) {
+    this.titre ='app works';
+    this.firsName ="robert";
+    this.name = "dupont";
+    this.collection = [ 
+      {reference:'1234',name:'toto',state:0},
+      {reference:'3456',name:'titi',state:1},
+      {reference:'5678',name:'tata',state:2},
+    ];
+    
+    this.testObservable();
+    this.resetNewItem();
+  }// fin constructeur
+  resetNewItem(){
 
-      this.testObservable();
-      }
-
+    this.newItem={reference:'',name:'',state:0};
+  }
+  getDetails(event:Event, parameter){
+    event.preventDefault();
+    console.log(event);
+    let eventEnCours = event.target || event.currentTarget || event.srcElement;
+    console.log(eventEnCours.attributes.id); //affiche id=1234
+    console.log(eventEnCours.id); // affiche 1234
+    console.log(parameter);
+   
+  }
+  
+  creationObjet(){
+   
+    this.collection.push(this.newItem);
+    console.log(this.newItem);
+    this.resetNewItem();
+  }
+      
   doClick(){
     this.name = "click sur button";
   }
@@ -44,8 +71,8 @@ export class HomePage {
     const myObservable = Observable.from([1,2,4,5])
     myObservable.subscribe((i) => console.log(i));
     
-    const myObservable2 = Observable.interval(1000)
-    myObservable2.subscribe((i) => (console.log(i)));
+    //const myObservable2 = Observable.interval(1000)
+    //myObservable2.subscribe((i) => (console.log(i)));
 
     // observation des Event
     window.onload= function(){
@@ -60,4 +87,4 @@ export class HomePage {
   
 
 
-}
+}//fin constructeur
